33196fa357de51bb4b891c3b8140a5c73dd3207a
xadicha <246abdurashidova123@gmail.com>
2024-02-11 18:51:22 +0500

    my_realloc in progress

--- readline.c
+++ readline.c
@@ -37,6 +37,24 @@ char* strn_cpy(char* dest, char* source, int length)
     return dest;
 }
 
+char* str_cpy(char* dest, char* source)
+{
+    for (int i = 0; source[i] != '\0'; i++)
+    {
+       dest[i] = source[i];
+    }
+    
+    return dest;
+}
+
+void fill_with_nl(char* str, int size)
+{
+    for (int i = 0; i < size; i++)
+    {
+        str[i] = '\0';
+    }
+}
+
 char* str_cat(char* main_str, char* str)
 {
     // to continue printing next lines
@@ -54,50 +72,104 @@ char* str_cat(char* main_str, char* str)
     return main_str;
 }
 
-char *my_readline(int fd)
+char* my_realloc(char* str, int size)
 {
-    if (STORAGE_OF_FILE == NULL)
-    {
-        STORAGE_OF_FILE = calloc(READLINE_READ_SIZE + 1, sizeof(char));
-    }
-    int index_of_storage = 0;
-    int flag_n;
+    char* new_str = malloc(size);
 
-    while ((flag_n = check_n(STORAGE_OF_FILE)) != FAILURE)
-    {
-        int reader = read(fd,&STORAGE_OF_FILE[index_of_storage], READLINE_READ_SIZE);
-        if (reader == 0)
-        {
-            return STORAGE_OF_FILE;
-        }
-        index_of_storage += reader;
+    if (size <= 0) {
+        return NULL;
     }
 
-    char* ready_line = calloc(flag_n + 1, sizeof(char));
-    strn_cpy(ready_line, STORAGE_OF_FILE, flag_n);
+    if(new_str == NULL)
+    {
+        return NULL;
+    }
 
-    char* next_lines = calloc(my_strlen(ready_line) + 1, sizeof(char));
-    str_cat(next_lines, STORAGE_OF_FILE);
-   
-    return next_lines;
+    if (str != NULL)
+    {
+        str_cpy(new_str, str);
+        free(str);
+    }
+    return new_str;    
 }
 
-int main(int ac, char av)
+int file_size(char* str)
+{
+    int fd = open(str, O_RDONLY);
+    char letter;
+    int counter = 0;
+    while (read(fd, &letter, 1))
+    {
+        counter++;
+    }
+
+    return counter;
+}
+
+void init_my_readline()
+{
+    if (STORAGE_OF_FILE)
+        free(STORAGE_OF_FILE);
+
+    // int counter = file_size()
+    STORAGE_OF_FILE = malloc((READLINE_READ_SIZE + 1) * sizeof(char));
+    fill_with_nl(STORAGE_OF_FILE, READLINE_READ_SIZE + 1);
+}
+
+char *my_readline(int fd)
+{
+    if (fd == -1)
+        return NULL;
+
+    int index = 0;
+    if (my_strlen(STORAGE_OF_FILE) == 0)
+    {
+        while (read(fd, &STORAGE_OF_FILE[index], READLINE_READ_SIZE))
+        {
+            index += READLINE_READ_SIZE;
+            int allocate_size = READLINE_READ_SIZE + index + 1;
+            STORAGE_OF_FILE =(char*) realloc(STORAGE_OF_FILE, allocate_size);
+            // fill_with_nl(STORAGE_OF_FILE, (READLINE_READ_SIZE + index + 1));
+        }
+    }
+    char* ret_value;
+    int n_index = check_n(STORAGE_OF_FILE);
+    if (n_index == FAILURE)
+    {
+        int length = my_strlen(STORAGE_OF_FILE);
+        if (my_strlen(STORAGE_OF_FILE) == 0)
+        {
+            return NULL;
+        }
+        ret_value = malloc((my_strlen(STORAGE_OF_FILE) + 1) * sizeof(char));
+        fill_with_nl(ret_value, (my_strlen(STORAGE_OF_FILE) + 1));
+        ret_value = strn_cpy(ret_value, STORAGE_OF_FILE, my_strlen(STORAGE_OF_FILE));
+        STORAGE_OF_FILE += length;
+        return ret_value;
+    }
+    else
+    {
+        ret_value = malloc((n_index + 2) * sizeof(char));
+        fill_with_nl(ret_value, (n_index + 2));
+        ret_value = strn_cpy(ret_value, STORAGE_OF_FILE, n_index);
+        STORAGE_OF_FILE += n_index + 1;
+        return ret_value;
+    }
+}
+
+int main(int ac, char** av)
 {
     char *str = NULL;
 
-    int fd = open("./example.txt", O_RDONLY);
+    init_my_readline();
+    int fd = open(av[1], O_RDONLY);
     while ((str = my_readline(fd)) != NULL)
     {
+        // sleep(1);
         printf("%s\n", str);
         free(str);
     }
     close(fd);
-
-    // if (STORAGE_OF_FILE == NULL)
-    // {
-    //     printf("%d\n", my_strlen("12345678910"));
-    // }
 
     return 0;
 }
--- test.c
+++ test.c
@@ -38,16 +38,47 @@ char* str_cpy(char* dest, char* source)
     return dest;
 }
 
+char* my_realloc(char* str, int size)
+{
+    char* new_str = malloc(size);
+
+    if (size <= 0) {
+        return NULL;
+    }
+
+    if(new_str == NULL)
+    {
+        return NULL;
+    }
+
+    if (str != NULL)
+    {
+        str_cpy(new_str, str);
+        free(str);
+    }
+    return new_str;    
+}
+
+
 int main(void)
 {
-    char arr1[20] = "Hello, ";
-    char* arr2 = "World!";
-    str_cat(arr1, arr2);
-    printf("%s\n", arr1);
+    // char arr1[20] = "Hello, ";
+    // char* arr2 = "World!";
+    // str_cat(arr1, arr2);
+    // printf("%s\n", arr1);
     // strcat(arr1, arr2);
     // printf("%s\n", arr1);
 
-    char* arr3 = calloc(10, sizeof(char));
-    str_cpy(arr3, "hi");
-    printf("%s\n", arr3);
+    // char* arr3 = calloc(10, sizeof(char));
+    // str_cpy(arr3, "hi");
+    // printf("%s\n", arr3);
+
+
+    char* str = malloc(sizeof(char) * 2);
+
+    str = my_realloc(str, 10);
+    str_cpy(str, "hellokjefnbr");
+
+    printf("%s\n", str);
+
 }
